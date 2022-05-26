6994c74c7cd0e2177e3ecfe0396c1716346f382e
Ricardo R <rretamal.dev@gmail.com>
2022-05-26 09:13:40 -0400

    Initial files

---
+++ Maui.DogClassifier.Core/Platforms/Android/Classifier.cs
@@ -0,0 +1,34 @@
+﻿using Java.IO;
+using Java.Nio;
+using Java.Nio.Channels;
+
+namespace Maui.DogClassifier.Core
+{
+    // All the code in this file is only included on Android.
+    public class Classifier
+    {
+        private MappedByteBuffer model;
+
+        public Classifier()
+        {
+            model = LoadModel();
+            
+        }
+
+        public void GetLabel()
+        { 
+        }
+
+        private MappedByteBuffer LoadModel()
+        {
+            var assets = Android.App.Application.Context.Assets;
+
+            var assetDescriptor = assets.OpenFd("model.tflite");
+            var inputStream = new FileInputStream(assetDescriptor.FileDescriptor);
+
+            var mappedByteBuffer = inputStream.Channel.Map(FileChannel.MapMode.ReadOnly, assetDescriptor.StartOffset, assetDescriptor.DeclaredLength);
+
+            return mappedByteBuffer;
+        }
+    }
+}
---
+++ Maui.DogClassifier/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.DogClassifier;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
